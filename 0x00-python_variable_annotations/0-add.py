ae0be6766f544d23b21acaa9262f43a092be06f2
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-03 22:52:49 +0100

    Update variable annotation1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
+"""Module-level documentation."""
+
+
 def add(a: float, b: float) -> float:
     """Adds two floats and returns their sum.
+
+    Args:
+        a (float): The first number to be added.
+        b (float): The second number to be added.
+
+    Returns:
+        float: The sum of a and b.
     """
-    return (a + b)
+    return a + b
