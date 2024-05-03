6777bab8610b428ff41adbceb7e688b3b42d83fb
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-04 00:49:35 +0100

    Update V_annotation1

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,5 +6,6 @@ from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Returns a list of tuples containing elements from lst and their lengths."""
+    """Returns a list of tuples containing elements from lst and their lengths
+    """
     return [(i, len(i)) for i in lst]
