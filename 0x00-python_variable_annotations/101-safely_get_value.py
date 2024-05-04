b49c0085c1afc2e47507be8875b66f43b85d39a0
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-04 01:06:24 +0100

    Update V_annotation2

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,8 @@ from typing import Mapping, Any, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any,
+                                                                         T]:
     """Returns the value associated with the given key in the dictionary.
     If the key is not present, returns the default value."""
     if key in dct:
