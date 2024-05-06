322b661c9d99094570830472a30af528c8fabea5
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-06 21:53:38 +0100

    pawns wait random

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns wait_random n times with the specified max_delay and returns the list of delays.
+    Spawns wait_random n times with the specified max_delay and returns the
+    list of delays.
     """
     return await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
