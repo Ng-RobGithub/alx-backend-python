d5437c966e0ed8cab6177b959f18f8633f193405
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-06 21:48:14 +0100

    Wait randomly

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,27 +1,13 @@
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
 import random
 
 
-@asyncio.coroutine
-def wait_random(max_delay=10):
-    """
-    Coroutine that waits for a random delay between 0 and max_delay seconds.
-
-    Args:
-        max_delay (int, optional): The maximum delay in seconds.
-        Defaults to 10.
-
-    Returns:
-        float: The random delay.
-    """
-    delay = random.uniform(0, max_delay)
-    yield asyncio.sleep(delay)
-    yield delay
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    print(loop.run_until_complete(wait_random()))
-    print(loop.run_until_complete(wait_random(5)))
-    print(loop.run_until_complete(wait_random(15)))
-    loop.close()
+async def wait_random(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
