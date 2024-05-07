3167eee7045fc0fb58cb962335457f03489d1253
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-07 21:39:23 +0100

    Total Runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,35 +1,18 @@
 #!/usr/bin/env python3
-""" coroutine that will measure the total runtime and return it"""
-
+'''coroutine that will measure the total runtime and return it
+'''
 import asyncio
+import time
 from importlib import import_module as using
+
 
 async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """
-    Measures the total runtime for executing async_comprehension four times in
-    parallel.
-
-    This coroutine executes async_comprehension four times in parallel
-    using asyncio.gather(). It measures the total runtime for all four
-    executions and returns it.
-
-    Returns:
-        float: The total runtime for executing async_comprehension four times
-        in parallel.
-    """
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
     start_time = asyncio.get_event_loop().time()
-
-    # Execute async_comprehension four times in parallel
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    end_time = asyncio.get_event_loop().time()
-
-    return end_time - start_time
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return asyncio.get_event_loop().time() - start_time
