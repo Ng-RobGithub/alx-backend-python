74c5ef7859aa7903d6c98bcc4ea550b361dfa0a8
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-07 21:22:04 +0100

    Total Runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,6 @@
 """ coroutine that will measure the total runtime and return it"""
 
 import asyncio
-import time
 from importlib import import_module as using
 
 async_comprehension = using('1-async_comprehension').async_comprehension
@@ -31,6 +30,6 @@ async def measure_runtime() -> float:
         async_comprehension()
     )
 
-    """end_time = asyncio.get_event_loop().time()"""
+    end_time = asyncio.get_event_loop().time()
 
-    return asyncio.get_event_loop().time() - start_time
+    return end_time - start_time
